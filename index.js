2b14a617ac03beddb39a5492358cc0d7f5b6457d
Stefan Penner <stefan.penner@gmail.com>
2015-03-15 16:17:40 -0700

    ensure the template file is not included

--- index.js
+++ index.js
@@ -29,28 +29,38 @@ Template.prototype.currentTemplateFile = function() {
   var stats = fs.statSync(this.templatePath);
 
   return new TemplateFile(this.templatePath, stats.isFile() ? 'file' : 'directory', stats);
-}
+};
 
 Template.prototype.processString = function(file, relativePath) {
   return this._template.template(this.variablesFn(file, relativePath));
-}
+};
+
+Template.prototype.isTemplatePath = function(path) {
+  return this.templatePath === '/' + path;
+};
+
+Template.prototype.canProcessFile = function(relativePath) {
+  if (this.isTemplatePath(relativePath)) { return true; }
+
+  return Filter.prototype.canProcessFile.call(this, relativePath);
+};
 
 Template.prototype.processAndCacheFile = function(srcDir, destDir, relativePath) {
+  if (this.isTemplatePath(relativePath)) { return; }
+
   if (this._template === undefined) {
     this._template = this.currentTemplateFile();
   } else {
     var newTemplateFile = this.currentTemplateFile();
-    if (newTemplateFile.equal(this._template)) {
-      return;
+
+    if (!newTemplateFile.equal(this._template)) {
+      this._template = newTemplateFile;
+      this.flushCache();
     }
-
-    this._template = newTemplateFile;
-
-    this.flushCache();
   }
 
   return Filter.prototype.processAndCacheFile.call(this, srcDir, destDir, relativePath);
-}
+};
 
 Template.prototype.flushCache = function() {
   delete this._cache;
