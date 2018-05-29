6fbef32cc8c83078f8c3bc709f70504cfc1a99a2
Stefan Penner <stefan.penner@gmail.com>
2018-05-29 10:48:12 -0700

    improve tests

--- tests/index.js
+++ tests/index.js
@@ -6,45 +6,114 @@ const broccoli = require('broccoli');
 const Template = require('..');
 const walkSync = require('walk-sync');
 const fs = require('fs');
+const { createBuilder, createTempDir } = require('broccoli-test-helper');
+const co = require('co');
 
 chai.use(require('chai-fs'));
 
+
+const TEMPLATE = `
+(function (global) {
+  define('fetch', [ 'ember', 'exports' ], function(Ember, self) {
+    'use strict';
+    var Promise = Ember['default'].RSVP.Promise;
+    if (global.FormData) {
+      self.FormData = global.FormData;
+    }
+    if (global.FileReader) {
+      self.FileReader = global.FileReader;
+    }
+    if (global.Blob) {
+      self.Blob = global.Blob;
+    }
+
+    <%= moduleBody %>
+
+    self['default'] = self.fetch;
+  });
+
+  define('fetch/ajax', [ 'fetch', 'exports' ], function(fetch, exports) {
+    'use strict';
+
+    exports['default'] = function() {
+      return fetch['default'].apply(fetch, arguments).then(function(request) {
+        return request.json();
+      });
+    };
+  });
+}(typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : this));`
+
 describe('BroccoliTemplater', function() {
   describe('builds', function() {
-    let builder;
+    let input;
+    let template;
+    let output;
 
-    function template(tree, templatePath, callbackForValues) {
-      builder = new broccoli.Builder(new Template(tree, templatePath , callbackForValues));
-      return builder;
-    }
+    beforeEach(co.wrap(function* () {
+      input = yield createTempDir();
+      template = yield createTempDir();
 
-    function filesWithin(result) {
-      return walkSync(result.directory).filter(file => {
-        return file.charAt(file.length - 1) !== '/'
-      }).map(p => {
-        return p.replace(__dirname, '')
-      });
-    }
-
-    after(function() {
-      builder.cleanup();
-    });
-
-    it('basic templating', function() {
-      let tree = template(__dirname + '/fixtures/one', __dirname + '/fixtures/templates/module-template.js.t', (content, relativePath) => {
+      let tree = new Template(input.path(), template.path() + '/module-template.js.t', (content, relativePath) => {
         return {
           moduleBody: content
         };
-      })
-
-      return tree.build().then(() => {
-        let path = tree.outputPath + '/foo.js';
-
-        expect(path).to.be.a.file;
-        expect(path).to.have.content.that.match(/'fetch\/ajax'/);
       });
-    });
+
+      output = createBuilder(tree);
+
+      template.write({
+        'module-template.js.t': TEMPLATE
+      });
+    }));
+
+    after(co.wrap(function* () {
+      yield input.dispose();
+      yield template.dispose();
+      yield output.dispose();
+    }));
+
+    it('basic templating', co.wrap(function* () {
+      input.write({
+        'foo.js': `
+function foo() {
+
+}
+        `
+      });
+
+      yield output.build();
+
+      expect(output.read()['foo.js']).to.match(/'fetch\/ajax'/);
+    }));
+
+    it('stability', co.wrap(function* () {
+      input.write({
+        'foo.js': `
+function foo() {
+
+}
+        `
+      });
+
+      yield output.build();
+      expect(output.changes()).to.eql({ 'foo.js': 'create' });
+
+input.write({
+        'foo.js': `
+function bar() {
+
+}
+        `
+      });
+
+      yield output.build();
+      expect(output.changes()).to.eql({ 'foo.js': 'change' });
 
 
+      yield output.build();
+
+      // TODO: expect no changes, inputs haven't changed, so our output should also not.
+      // expect(output.changes()).to.eql({ });
+    }));
   });
 });
